6518b50ad0761ece77908a7e8f4cac501a992b75
Iago <iago-neves8@hotmail.com>
2020-11-30 17:57:14 -0300

    Adicoes no template

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -4,17 +4,26 @@
       <nav-bar logo="Cookies da Maha" url="#"/>    
     </header>
     <main>
+<div class="container"> 
+<div class="row">
+      <div class="container"> 
+        <grid-vue tamanho="12"
+          <div class="img-transparent">
+              <img src="https://media.discordapp.net/attachments/779117764199579655/779118361430327326/image0.png?width=556&height=554" alt="" class="circle responsive-img">
+          </div>
+            </div>
+        </grid-vue>
+    <router-view/>  
+</div>
+</div>
+
 <div class="container">
 
-  <div class="row">
-    <grid-vue tamanho="12">
-    <img src="https://media.discordapp.net/attachments/779117764199579655/779118361430327326/image0.png?width=556&height=554" alt="" class="circle responsive-img">
-    <router-view/>
-    </grid-vue>
-      
-  </div>
+<cardmenuVue/>
 
 </div>
+
+
 
     </main>
 
@@ -47,5 +56,13 @@ export default {
 </script>
 
 <style>
+
+.img-transparent {
+  opacity : 0.5;
+}
+
+.back-color {
+  background-color:rgb(253, 243, 243);
+}
 
 </style>
